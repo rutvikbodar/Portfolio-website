51824ebe7b440c36e0e93054680293ba9cda9c6e
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-08-06 14:38:40 +0530

    jarvis injected

--- src/Skill_container.js
+++ src/Skill_container.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Hexagon from "./Hexagon";
+import Jarvis from "./Jarvis";
 import './Skill_container.css';
 export default function Skill_container(){
     return (
@@ -25,38 +26,7 @@ export default function Skill_container(){
                 <Hexagon hexagonid={"mysql_hexagon"} imgsrc={"mysql.png"} />
             </div>
         </div>
-        <div id="hive_addons">
-           <div id="choose-skill">
-            <div className="choose-skill-l1"></div>
-                <div className="arc">
-                    <div className="temp"></div>
-                    <div className="temp_cover"></div>
-                </div>
-                <div className="tri-circle">
-                    <div className="tri" id="tri1"></div>
-                    <div className="tri" id="tri2"></div>
-                    <div className="tri" id="tri3"></div>
-                    <div className="tri" id="tri4"></div>
-                    <div className="tri" id="tri5"></div>
-                    <div className="tri" id="tri6"></div>
-                    <div className="tri" id="tri7"></div>
-                    <div className="tri" id="tri8"></div>
-                    <div className="tri" id="tri9"></div>
-                    <div className="tri" id="tri10"></div>
-                    <div className="tri" id="tri11"></div>
-                    <div className="tri" id="tri12"></div>
-                    <div className="tri" id="tri13"></div>
-                    <div className="tri" id="tri14"></div>
-                    <div className="tri" id="tri15"></div>
-                    <div className="tri" id="tri16"></div>
-                    <div className="tri" id="tri17"></div>
-                    <div className="tri" id="tri18"></div>
-                </div>
-                <div className="choose-skill-center-container">
-                    <img src="chooseskill.png" alt="Loading..." className="choose-skill-center-image" />
-                </div>
-            </div>
-        </div>
+        <Jarvis />
     </div>
         </>
     )
