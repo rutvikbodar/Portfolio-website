205788d0a675a365978af29870eb2608c8f353ba
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-08-07 22:44:43 +0530

    percentage indicater added

--- src/Skill_states.js
+++ src/Skill_states.js
@@ -25,6 +25,19 @@ export default function Skill_states(props){
                         <div className="percentageLayer" style={percentageLayerStyle}></div>
                     </div>
                     <p className="masteryLabel">Mastery Level</p> 
+                    <div className="percentageIndicator">
+                        <div className="indicatorValue">0</div>
+                        <div className="indicatorValue">10</div>
+                        <div className="indicatorValue">20</div>
+                        <div className="indicatorValue">30</div>
+                        <div className="indicatorValue">40</div>
+                        <div className="indicatorValue">50</div>
+                        <div className="indicatorValue">60</div>
+                        <div className="indicatorValue">70</div>
+                        <div className="indicatorValue">80</div>
+                        <div className="indicatorValue">90</div>
+                        <div className="indicatorValue">100</div>
+                    </div>
                 </div>
             </div>
         </div>
