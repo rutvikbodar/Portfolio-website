13bb5f8de8dc0412704724abb60c20e9cb6914bc
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-08-20 17:09:29 +0530

    db connected to frontend v2

---
+++ frontend/src/DAO/skillDAO.js
@@ -0,0 +1,8 @@
+
+import axios from 'axios';
+
+export default async function retriveSkillData(){
+    const skills = (await axios.get('http://localhost:5000/')).data;
+    console.log(skills);
+    return skills;
+}
