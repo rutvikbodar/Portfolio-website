1366ea0fe397cd11b35b30b99fb3e7781bbcf482
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-08-20 22:45:24 +0530

    beta1

--- frontend/src/Skill_container.js
+++ frontend/src/Skill_container.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hexagon from "./Hexagon";
 import Jarvis from "./Jarvis";
 import Skill_state from './Skill_states.js';
@@ -8,8 +8,13 @@ import retriveSkillData from "./DAO/skillDAO";
 
 
 export default function Skill_container(){
+    var factor = (window.innerWidth>700?1:2); 
+    console.log(`skill container is built and the new factor value is : ${factor}`);
+    function createStyle(index){
+        const devidend = Math.ceil(index/4);
+        const remain = index%4;
+    }
 
-    retriveSkillData();
     var [isInitiated,setIsInitiated] = React.useState(false);
     var[skillObject,setSkillObject] = React.useState({"skillName" : "","imagePath" : ""});
 
@@ -17,31 +22,38 @@ export default function Skill_container(){
         console.log("init..........");
         setSkillObject({"skillName" : skillName,"imagePath" : imagePath});
         setIsInitiated(true);
-        
     }
+    useEffect(()=>{
+        retriveSkillData();
+        window.addEventListener('resize',()=>{
+            if(window.innerWidth>700 && factor===2){
+                factor=1;
+                console.log(factor);
+            }
+            if(window.innerWidth<700 && factor===1){
+                factor=2;
+                console.log(factor);
+            }
+        });
+    },[]);
+    
 
     return (
         <>
             <div className="center">
                 <div className="skillhive">
-                    <div className="row1">
-                        <Hexagon skillName = {"Flutter"} hexagonid={"flutter_hexagon"} imgsrc={"flutter.svg"} initialize={initialize} />
-                        <Hexagon skillName = {"Java"} hexagonid={"java_hexagon"} imgsrc={"java.png"} initialize={initialize} />
-                        <Hexagon skillName = {"Firebase"} hexagonid={"firebase_hexagon"} imgsrc={"firebase.png"} initialize={initialize} />
-                        <Hexagon skillName = {"C#"} hexagonid={"cs_hexagon"} imgsrc={"cs.png"} initialize={initialize} />
-                    </div>
-                    <div className="row2">
-                        <Hexagon skillName = {"CSS"} hexagonid={"css_hexagon"} imgsrc={"css.png"} initialize={initialize} />
-                        <Hexagon skillName = {"HTML"} hexagonid={"html_hexagon"} imgsrc={"html.png"} initialize={initialize} />
-                        <Hexagon skillName = {"Javascript"} hexagonid={"js_hexagon"} imgsrc={"js.png"} initialize={initialize} />
-                        <Hexagon skillName = {"Node.JS"} hexagonid={"nodejs_hexagon"} imgsrc={"nodejs.png"} initialize={initialize} />
-                    </div>
-                    <div className="row3">
-                        <Hexagon skillName = {"Bootstrap"} hexagonid={"bootstrap_hexagon"} imgsrc={"bootstrap.png"} initialize={initialize} />
-                        <Hexagon skillName = {"React.JS"} hexagonid={"reactjs_hexagon"} imgsrc={"reactjs.png"} initialize={initialize} />
-                        <Hexagon skillName = {"MongoDB"} hexagonid={"mongodb_hexagon"} imgsrc={"mongodb.png"} initialize={initialize} />
-                        <Hexagon skillName = {"MySQL"} hexagonid={"mysql_hexagon"} imgsrc={"mysql.png"} initialize={initialize} />
-                    </div>
+                    <Hexagon skillName = {"Flutter"} hexagonid={"flutter_hexagon"} imgsrc={"flutter.svg"} initialize={initialize} />
+                    <Hexagon skillName = {"Java"} hexagonid={"java_hexagon"} imgsrc={"java.png"} initialize={initialize} />
+                    <Hexagon skillName = {"Firebase"} hexagonid={"firebase_hexagon"} imgsrc={"firebase.png"} initialize={initialize} />
+                    <Hexagon skillName = {"C#"} hexagonid={"cs_hexagon"} imgsrc={"cs.png"} initialize={initialize} />
+                    <Hexagon skillName = {"CSS"} hexagonid={"css_hexagon"} imgsrc={"css.png"} initialize={initialize} />
+                    <Hexagon skillName = {"HTML"} hexagonid={"html_hexagon"} imgsrc={"html.png"} initialize={initialize} />
+                    <Hexagon skillName = {"Javascript"} hexagonid={"js_hexagon"} imgsrc={"js.png"} initialize={initialize} />
+                    <Hexagon skillName = {"Node.JS"} hexagonid={"nodejs_hexagon"} imgsrc={"nodejs.png"} initialize={initialize} />
+                    <Hexagon skillName = {"Bootstrap"} hexagonid={"bootstrap_hexagon"} imgsrc={"bootstrap.png"} initialize={initialize} />
+                    <Hexagon skillName = {"React.JS"} hexagonid={"reactjs_hexagon"} imgsrc={"reactjs.png"} initialize={initialize} />
+                    <Hexagon skillName = {"MongoDB"} hexagonid={"mongodb_hexagon"} imgsrc={"mongodb.png"} initialize={initialize} />
+                    <Hexagon skillName = {"MySQL"} hexagonid={"mysql_hexagon"} imgsrc={"mysql.png"} initialize={initialize} />
                 </div>
                 <div id="hive_addons">
                     {isInitiated?<Skill_state skillName={skillObject.skillName} imagePath={skillObject.imagePath} proficiency={Data[skillObject.skillName]["Proficiency"]} percentage={Data[skillObject.skillName]["MasteryLevel"]} numberOfProjects={Data[skillObject.skillName]["NumberOfProjects"]} opacity={0} id={skillObject.skillName}/>:<Jarvis />}
