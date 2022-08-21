ea9f05ad7b4350e3cf5288462108b2dd623ad3f4
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-08-21 18:28:11 +0530

    homepage initial setup

---
+++ frontend/src/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Homepage.css';
+import Navbar from "./Navbar";
+
+export default function Homepage(){
+    return(
+        <div className="homepage">
+            <Navbar />
+        </div>
+    )
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Skill_container from './Skill_container';
+import Homepage from './Homepage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <Skill_container />
-  </>
+  <div className='index'>
+    <Homepage />
+  </div>
 );
 
