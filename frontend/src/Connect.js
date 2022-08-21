9a8d3ebbac8691b85155aaf2e93a58fbe1c8982e
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-08-21 23:35:46 +0530

    connection list frontend prepared

--- frontend/src/Connect.js
+++ frontend/src/Connect.js
@@ -7,17 +7,19 @@ export default function Connect(){
         (hidden)?hidden=false:hidden=true;
         console.log(hidden);
     }
+    
 
     return (
         <>
             <div className="connect" onClick={toggle}>
-            <img src="connect.jpg" className="connectImage"></img>
-            <div className="connection"></div>
-            <div className="connection"></div>
-            <div className="connection"></div>
-            <div className="connection"></div>
-            <div className="connection"></div>
-        </div>
+                <img src="connect.jpg" className="connectImage"></img>
+            </div>
+            <div className="instagram"><img src="insta.png" className="connectionImage" id="instagram"></img></div>
+            <div className="facebook"><img src="fb.png" className="connectionImage" id="facebook"></img></div>
+            <div className="linkedin"><img src="linkedin.png" className="connectionImage" id="linkedin"></img></div>
+            <div className="email"><img src="email.png" className="connectionImage" id="email"></img></div>
+            <div className="tweeter"><img src="tweeter.png" className="connectionImage" id="tweeter"></img></div>
+            <div className="github"><img src="github.png" className="connectionImage" id="github"></img></div>
         </>
         
     )
