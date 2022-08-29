b90c21dd34ddf0ec3205a93df1b03b9822ed1a54
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-08-29 15:58:11 +0530

    project container layout

--- frontend/src/ProjectCard.js
+++ frontend/src/ProjectCard.js
@@ -10,7 +10,25 @@ export default function ProjectCard(props){
                     <img className="projectImage" src="portfolioProject.png" />
                 </div>
             </div>
-            <div className="ProjectDes"></div>
+            <div className="ProjectDes">
+                <div className="titleContainer">
+                    <div className="projectName">
+                        Portfolio Website
+                    </div>
+                    <div className="projectVersion">
+                        V 1
+                    </div>
+                </div>
+                <div className="projectDescription">
+                    <ul>
+                        <li>
+                        A full stack animation rich web application to represent my portfolio in a creative and interactive way.
+                        </li>
+                        <li>Technologies used : <span className="highlight"> M.E.R.N</span></li>
+                        <li><span className="highlight">Creativity</span>: the application is heavely backed by various algorithms that generates new design and styling according to data. for instance, you just need to add new skill to database and algorithms will take care of positioning and animations.</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
