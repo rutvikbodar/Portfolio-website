a43ada6a8129157ba8cdc4806bb75f11ae814e06
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-08-30 22:09:30 +0530

    frames aligned

--- frontend/src/ProjectCard.js
+++ frontend/src/ProjectCard.js
@@ -3,11 +3,23 @@ import './ProjectCard.css';
 
 export default function ProjectCard(props){
     var style = (props.index%2==0)?{marginLeft : "3vw"}:{marginLeft : "35vw"}
+    var frameStyle;
+    var projectImageContainer;
 
+    if(props.type==="web"){
+        frameStyle={"height":"100%","width":"100%"};
+        projectImageContainer={"position" : "absolute","backgroundColor" : "red","marginTop":"4%","marginLeft":"0%","height":"71.5%","width":"92%"}
+    }
+    else{
+        frameStyle={"height":"100%"};
+        projectImageContainer={"position" : "absolute","backgroundColor" : "red","marginTop":"8.45%","height":"80%","width":"39%"}
+    }
+    
+    
     
     var projectImage = (<div className="projectImageCover" key={"projectImageCover"}>
-                            <img src={(props.type==="web")?"desktop.png":"phone.png"} className="projectImage"/>
-                            <div className="projectImageContainer">
+                            <img src={(props.type==="web")?"desktop.png":"phone.png"} className="projectframe"/>
+                            <div  style={projectImageContainer}>
                                 <img className="projectImage" src={props.img} />
                             </div>
                         </div>);
