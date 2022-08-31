7d64082ec2355dd41ac207f69ac0a8f51cd61c77
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-08-31 17:05:37 +0530

    toggleComplete

--- frontend/src/ProjectCard.js
+++ frontend/src/ProjectCard.js
@@ -5,6 +5,8 @@ export default function ProjectCard(props){
     var style = (props.index%2==0)?{marginLeft : "3vw"}:{marginLeft : "35vw"}
     var frameStyle;
     var projectImageContainer;
+    var [versionCardStyle,setVersionCardStyle] = React.useState((props.index%2===0)?{"marginLeft" : "35vw"}:{"marginLeft" : "45vw"});
+    var [visible,setVisible] = React.useState(false);
 
     if(props.type==="web"){
         frameStyle={"height":"100%","width":"100%"};
@@ -14,8 +16,30 @@ export default function ProjectCard(props){
         frameStyle={"height":"100%"};
         projectImageContainer={"position" : "absolute","backgroundColor" : "red","marginTop":"8.45%","height":"80%","width":"39%"}
     }
-    
-    
+
+    var toggleVersions = ()=>{
+        if(props.index%2===0){
+            if(!visible){
+                setVersionCardStyle({"marginLeft" : "65vw"});
+                setVisible(true);
+            }
+            else{
+                setVersionCardStyle({"marginLeft" : "35vw"});
+                setVisible(false);
+            }
+        }
+        else{
+            if(!visible){
+                setVersionCardStyle({"marginLeft" : "15vw"});
+                setVisible(true);
+            }
+            else{
+                setVersionCardStyle({"marginLeft" : "45vw"});
+                setVisible(false);
+            }
+        }
+        console.log(versionCardStyle);
+    }
     
     var projectImage = (<div className="projectImageCover" key={"projectImageCover"}>
                             <img src={(props.type==="web")?"desktop.png":"phone.png"} className="projectframe"/>
@@ -29,7 +53,7 @@ export default function ProjectCard(props){
                             <div className="projectName">
                                 {props.name}
                             </div>
-                            <div className="projectVersion">
+                            <div className="projectVersion" onClick={toggleVersions}>
                                 {props.version}
                             </div>
                         </div>
@@ -48,10 +72,15 @@ export default function ProjectCard(props){
                     </div>);
 
     return (
-        <div className="projectCard" style={style}>
-            {(props.index%2==0)
-            ?[projectImage,projectDes]
-            :[projectDes,projectImage]}
+        <div>
+            <div className="versionCard" style={versionCardStyle}>
+                
+            </div>
+            <div className="projectCard" style={style}>
+                {(props.index%2==0)
+                ?[projectImage,projectDes]
+                :[projectDes,projectImage]}
+            </div>
         </div>
     )
 }
