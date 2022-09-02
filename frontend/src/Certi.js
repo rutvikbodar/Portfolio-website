a135f351f876f4086a935e1ec13dc5fcd029bfd3
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-02 19:54:23 +0530

    certi v1 complete

--- frontend/src/Certi.js
+++ frontend/src/Certi.js
@@ -5,7 +5,7 @@ export default function Certi(){
     return(
         <div className='certi'>
             <div className='name'>Microsoft Azure AZ-900</div>
-            <div className='org'>By Microsoft Corporation <div className='orgLogo'></div></div>
+            <div className='org'><span className='by'>By </span><div className='orgLogo'><img src='coursera.png' /></div></div>
             <div className='linkButton'>View certificate</div>
         </div>
     )
