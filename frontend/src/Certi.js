7cd64e4ebb954048dd999bfa14c6832e789e97f7
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-02 19:25:31 +0530

    logo container added

--- frontend/src/Certi.js
+++ frontend/src/Certi.js
@@ -5,7 +5,7 @@ export default function Certi(){
     return(
         <div className='certi'>
             <div className='name'>Microsoft Azure AZ-900</div>
-            <div className='org'>By Microsoft Corporation</div>
+            <div className='org'>By Microsoft Corporation <div className='orgLogo'></div></div>
             <div className='linkButton'>View certificate</div>
         </div>
     )
