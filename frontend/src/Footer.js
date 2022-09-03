9786743fe37b971d4c387a677370f190ff4eec34
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-03 18:57:37 +0530

    footer v3

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -10,7 +10,18 @@ export default function Footer(){
                 All the images, used in this websites are derived from open-source resources.<br />
                 This project is inspired by one of my favourite book, How to win friends and influnce people by Dale Carnegie. 
             </div>
-            <div className='links'></div>
+            <div className='links'>
+                <div className='connectFooter'>Contact us</div>
+                <br />
+                <div className='connectIconBar'>
+                    <div className='connectIcons'><img src='email.png' /></div>
+                    <div className='connectIcons'><img src='linkedin.png' /></div>
+                    <div className='connectIcons'><img src='insta.png' /></div>
+                    <div className='connectIcons'><img src='github.png' /></div>
+                    <div className='connectIcons'><img src='fb.png' /></div>
+                    <div className='connectIcons'><img src='tweeter.png' /></div>
+                </div>
+            </div>
         </div>
     )
 }
