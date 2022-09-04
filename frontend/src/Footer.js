0dde162c1db8c76733bcd9e441c04801fd404349
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-04 14:29:48 +0530

    footer complete

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -14,12 +14,12 @@ export default function Footer(){
                 <div className='connectFooter'>Contact us</div>
                 <br />
                 <div className='connectIconBar'>
-                    <a href='mailto:rutvik777.rb@gmail.com'><div className='connectIcons'><img src='email.png' /></div></a>
-                    <a href='https://www.linkedin.com/in/rutvik-bodar-bba927195/'><div className='connectIcons'><img src='linkedin.png' /></div></a>
-                    <a href='https://www.instagram.com/rutvikbodar/'><div className='connectIcons'><img src='insta.png' /></div></a>
-                    <a href='https://github.com/rutvikbodar'><div className='connectIcons'><img src='github.png' /></div></a>
-                    <a href='https://www.facebook.com/profile.php?id=100007320045966'><div className='connectIcons'><img src='fb.png' /></div></a>
-                    <a href='https://twitter.com/rutvik_bodar'><div className='connectIcons'><img src='tweeter.png' /></div></a>
+                    <a href='mailto:rutvik777.rb@gmail.com' target={"_blank"}><div className='connectIcons'><img src='emailColor.png' /></div></a>
+                    <a href='https://www.linkedin.com/in/rutvik-bodar-bba927195/' target={"_blank"}><div className='connectIcons'><img src='linkedinColor.png' /></div></a>
+                    <a href='https://www.instagram.com/rutvikbodar/' target={"_blank"}><div className='connectIcons'><img src='instaColor.png' /></div></a>
+                    <a href='https://github.com/rutvikbodar' target={"_blank"}><div className='connectIcons'><img src='githubColor.png' /></div></a>
+                    <a href='https://www.facebook.com/profile.php?id=100007320045966' target={"_blank"}><div className='connectIcons'><img src='fbColor.png' /></div></a>
+                    <a href='https://twitter.com/rutvik_bodar' target={"_blank"}><div className='connectIcons'><img src='tweeterColor.png' /></div></a>
                 </div>
                 <br />
                 <div>Email us : rutvik777.rb@gmail.com</div>
