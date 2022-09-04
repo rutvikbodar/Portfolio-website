d6849c52af1eeb57309fa22550dd1fb96177b6d8
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-04 16:48:38 +0530

    footer in about page

--- frontend/src/Aboutpage.js
+++ frontend/src/Aboutpage.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './Aboutpage.css';
+import Footer from './Footer';
 
 export default function AboutPage(){
     return(
-        <div className='aboutPage'></div>
+        <div>
+            <div className='aboutPage'>
+                <div className='coffeeContainer'>
+                    <img src={"coffee.png"} />
+                </div>
+            </div>
+            <Footer />
+        </div>
     )
 }
