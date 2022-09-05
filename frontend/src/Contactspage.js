4ef79ed2829a852cb107b960df83bd6ffb702585
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-05 22:11:03 +0530

    contact page frontend

--- frontend/src/Contactspage.js
+++ frontend/src/Contactspage.js
@@ -6,7 +6,16 @@ export default function Contactspage(){
     return (
         <div>
             <div className="contactspage">
-                contatcs
+                <div className="formBox">
+                    <div className="introMessage">Have an idea or want to work together?</div>
+                    <div className="fieldTag">Name</div>
+                    <input className={"textBox"} type={"text"} />
+                    <div className="fieldTag">Email</div>
+                    <input className={"textBox"} type={"text"} />
+                    <div className="fieldTag">Message</div>
+                    <textarea className="textArea"> </textarea>
+                    <div className="sendButton">Send</div>
+                </div>
             </div>
             <Footer />
         </div>
