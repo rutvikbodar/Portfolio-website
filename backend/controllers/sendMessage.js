b692562eb0fc020bd81a0903bd12136e42721e98
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-05 22:30:49 +0530

    send message backend succeed

--- backend/controllers/sendMessage.js
+++ backend/controllers/sendMessage.js
@@ -2,6 +2,12 @@ const client = require('../utils/mongoUtil');
 
 module.exports.sendMessage = async(req,res) =>{
     const {name,email,message} = req.body;
-    console.log(name,email,message);
-    res.end("post request received");
+    await client.connect();
+    const result = await client.db("Messages").collection("allMessages").insertOne({
+        "name" : name,
+        "email" : email,
+        "message" : message,
+        "time" : Date.now()
+    });
+    res.json(result);
 }
