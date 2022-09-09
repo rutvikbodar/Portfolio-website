33edaa40e9e1933a1650b5bbcce4762db4c0e0ea
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-09 16:52:34 +0530

    about section animated

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -7,7 +7,7 @@ import Contactspage from './Contactspage'
 
 export default function Main(){
 
-    var [currentPage,setCurrentPage] = React.useState("Aboutpage");
+    var [currentPage,setCurrentPage] = React.useState("Homepage");
 
     const providePage = (page)=>{
         if(page==="Homepage") return <Homepage key={"homepage"}/>
