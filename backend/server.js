749cbf336d184edcdc5cbc7facdc826a61f10e2f
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-10 16:01:40 +0530

    skill bug fix

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/',skillrouter);
+app.use('/Portfolio/allSkills',skillrouter);
 app.use('/Portfolio/numberOfProjects',numberOfProjectsRouter);
 app.use('/Portfolio/allProjects',allProjectsRoute);
 app.use('/Portfolio/experience',experienceRouter);
 app.use('/Protfolio/allCertificates',allCertificates);
 app.use('/contacts/sendmessage',sendMessageRouter);
+
+app.get('/',(req,res)=>{
+    res.send('lol');
+});
 
 
 app.listen(5000,()=>console.log("server is listning on port 5000......"));
--- frontend/src/DAO/skillDAO.js
+++ frontend/src/DAO/skillDAO.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 export default async function retriveSkillData(){
-    const skills = (await axios.get('http://localhost:5000/')).data;
+    const skills = (await axios.get('http://localhost:5000/Portfolio/allSkills')).data;
     return skills;
 }
