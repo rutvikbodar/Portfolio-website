1d7431828ea3a2e5afab6619347c8cb070b094ba
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-10 22:48:16 +0530

    half way

--- frontend/src/ProfileCover.js
+++ frontend/src/ProfileCover.js
@@ -2,8 +2,9 @@ import './ProfileCover.css';
 import React from 'react';
 
 export default function ProfileCover(){
+    var coverStyle = (window.innerHeight<1035)?{"animation": "rotatelAnimation 1s forwards"}:{}
     return(
-        <div className='ProfileCoverContainer'>
+        <div className='ProfileCoverContainer' style={coverStyle}>
             <img className='ProfileCover' src='./bodar.jpg'></img>
         </div>
     )
