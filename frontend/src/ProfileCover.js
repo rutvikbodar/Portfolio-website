8b1a8f04de6685d221e12aeda7f873a33db583e3
Rutvik Bodar <rutvik777.rb@gmail.com>
2022-09-10 23:05:05 +0530

    cover animation failed

--- frontend/src/ProfileCover.js
+++ frontend/src/ProfileCover.js
@@ -2,9 +2,8 @@ import './ProfileCover.css';
 import React from 'react';
 
 export default function ProfileCover(){
-    var coverStyle = (window.innerHeight<1035)?{"animation": "rotatelAnimation 1s forwards"}:{}
     return(
-        <div className='ProfileCoverContainer' style={coverStyle}>
+        <div className='ProfileCoverContainer'>
             <img className='ProfileCover' src='./bodar.jpg'></img>
         </div>
     )
